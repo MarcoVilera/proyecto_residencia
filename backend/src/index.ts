580b1a34df04d60be6fd566fe5b182d4df456401
Marco Vilera <MarcoVilArev123@gmail.com>
2024-06-23 21:31:16 -0400

    Add test route to API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,14 @@
 import express from 'express';
 import { sequelize } from './db.controller';
-
+import router from './routes/index';
 const app = express();
 const port = 3001;
 sequelize.sync();
 
 const ApiRouter = express.Router();
 app.use('/api', ApiRouter)
-
-app.get('/', (_req, res) => {
-    res.send({ message: 'Hello from Express!' });
-
-});
+//TEST, CAN BE DELETED
+ApiRouter.use('/', router)
 
 app.listen(port, () => {
     console.log(`Backend is running at http://localhost:3000/api, if only the backend is running is running on http://localhost:${port}/`);
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,11 @@
 /*
 Barrel file for routes
 */
+//TEST, CAN BE DELETED
+import express from 'express';
 
+const router = express.Router();
+
+export default router.get('/test', (_req, res) => {
+    res.send({ message: 'Hello from Express!' });
+});
