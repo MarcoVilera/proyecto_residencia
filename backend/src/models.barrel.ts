29db8ea0e314c308b22f592b91d7c8df5d7d2f7e
MarcoVilera <Marcovilarev123@gmail.com>
2024-06-26 09:46:04 -0400

    feat barrel file in order to get all models from a single file

---
+++ backend/src/models.barrel.ts
@@ -0,0 +1,15 @@
+//BARREL FILE FOR MODELS
+export * from "./models/Apartment";
+export * from "./models/Cash";
+export * from "./models/CommonArea_Reserve";
+export * from "./models/CommonArea";
+export * from "./models/MaintenanceRequest";
+export * from "./models/Notification";
+export * from "./models/Owner";
+export * from "./models/Payment";
+export * from "./models/Quota";
+export * from "./models/Resident";
+export * from "./models/Role";
+export * from "./models/ServicePayment";
+export * from "./models/User";
+
